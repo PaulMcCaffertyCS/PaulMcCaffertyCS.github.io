c8ca0aee4ebbafa1674f2b9944b206fff3db911b
Paul McCafferty <paulmccafferty@smarttech.com>
2025-06-25 21:15:40 -0400

    Initial module implementation attempts

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
+// import Point from "utils/javascript/Point";
+
 function boop() {
     alert("Boop!");
 }
 
-function moveButton() {
-    const button = document.getElementById("booperton");
-    const maxX = window.innerWidth - button.offsetWidth;
-    const maxY = window.innerHeight - button.offsetHeight;
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    button.style.position = "absolute";
-    button.style.left = `${randomX}px`;
-    button.style.top = `${randomY}px`;
+/**
+ * @param {HTMLElement} element Element desired to move.
+ * @param {Point} newLocation New location the element should go to.
+ */
+function moveElement(element, newLocation) {
+    element.style.position = "absolute";
+    element.style.left = `${newLocation.x}px`;
+    element.style.top = `${newLocation.y}px`;
 }
 
 /**
- * @param {HTMLElement} element Element desired to move.
+ * @param {HTMLElement|null} element Element for width/height, else null to just get a random Point in the Window.
+ * @returns {Point} A Point object containing the (x, y) location.
  */
-function randomLocation(element) {
+function getRandomLocationOnScreen(element) {
+    var maxX;
+    var maxY;
+    if (element == null) {
+        maxX = window.innerWidth;
+        maxY = window.innerHeight;
+    } else {
+        maxX = window.innerWidth - element.offsetWidth;
+        maxY = window.innerHeight - element.offsetHeight;
+    }
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+    return new Point(randomX, randomY);
+}
 
+function moveButton() {
+    const button = document.getElementById("booperton");
+    const randomLocation = getRandomLocationOnScreen(button);
+    moveElement(button, randomLocation);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
---
+++ utils/javascript/Point.js
@@ -0,0 +1,31 @@
+class Point {
+    /**
+     * @param {number} x X value.
+     * @param {number} y Y value.
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @param {Point} other A different Point to get the distance to.
+     * @returns {number} Distance from this Point to other Point.
+     */
+    distanceTo(other) {
+        const dx = this.x - other.x;
+        const dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    /**
+     * @param {number} dx Translate x by.
+     * @param {number} dy Translate y by.
+     */
+    translate(dx, dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+}
+
+export default Point;
