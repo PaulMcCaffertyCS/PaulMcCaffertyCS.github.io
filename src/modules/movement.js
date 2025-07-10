ee0eb083b87939205b78ab728afccd8b0a587066
Paul McCafferty <paulmccafferty@smarttech.com>
2025-07-10 11:11:05 -0400

    Implement new design structure

--- src/modules/movement.js
+++ src/modules/movement.js
@@ -107,9 +107,20 @@ function chaseElementWindow(event, element) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+<<<<<<< Updated upstream
     const button = document.getElementById("booperton");
     // button.addEventListener("mouseover", () => { randomElementLocation(button) });
     // button.addEventListener("mouseover", (event) => { chaseElement(event, button) });
     
     // document.addEventListener("mousemove", (event) => { chaseElementWindow(event, button) });
+=======
+    const pageLink = window.location.href;
+    if (pageLink.endsWith("games.html")) {
+        const button = document.getElementById("boop");
+        // button.addEventListener("mouseover", () => { randomElementLocation(button) });
+        // button.addEventListener("mouseover", (event) => { chaseElement(event, button) });
+        
+        document.addEventListener("mousemove", (event) => { chaseElementWindow(event, button) });
+    }
+>>>>>>> Stashed changes
 })
