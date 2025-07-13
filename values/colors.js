38d181b77642723b86b049775e42d5c75aeb0dd2
Paul McCafferty <paulmccafferty@smarttech.com>
2025-07-13 19:57:10 -0400

    Switch to React with TypeScript + SWC

--- values/colors.js
+++
@@ -1,9 +0,0 @@
-export default class Color {
-    static BLACK = rgb(0, 0, 0);
-    static SAGE_GREEN_LIGHTER_TONE_1 = rgb(6, 216, 93)
-    static SAGE_GREEN = rgb(10, 182, 76);
-    static SAGE_GREEN_DARKER_TONE_1 = rgb(14, 136, 61);
-    static SAGE_GREEN_DARKER_TONE_2 = rgb(16, 94, 46);
-    static TAN = rgb(250, 252, 225);
-    static WHITE = rgb(255, 255, 255);
-}
